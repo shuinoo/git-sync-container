d8bad98491386b6faef8717151cf879d8a469c7f
Samuel Bishop <sam@oneqone.com>
2018-03-06 16:55:46 +0800

    Adding an initial synchronization task
    - This means the cron job doesnt have to run as frequently in order to ensure a quick container setup during initial pod deployment.

---
+++ 30_intial_git_sync.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo 
+echo "Running Initial Git Synchronization..."
+set -x
+/opt/git_sync.sh 2>&1 | /usr/bin/logger -t initial_git_sync
+set +x
+echo "Initial Git Synchronization Complete!"
+echo
